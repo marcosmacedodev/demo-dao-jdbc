b0c1801b36482b5c7524fc72d7a2fb50e5438fc4
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-28 20:55:37 -0300

    Implementado findByDepartment, findAll

--- src/daos/impl/SellerDaoImpl.java
+++ src/daos/impl/SellerDaoImpl.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -41,13 +43,10 @@ public class SellerDaoImpl implements SellerDAO{
 
 	@Override
 	public Seller findById(Integer id) {
-		
-		
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		
 		try {
-			conn = DB.getConnection();
 			st = conn.prepareStatement(
 					"SELECT seller.*, department.Name as DepName "
 					+"FROM seller INNER JOIN department "
@@ -70,29 +69,86 @@ public class SellerDaoImpl implements SellerDAO{
 			throw new DbException(ex.getMessage());
 		}
 		finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-			if(st != null) {
-				try {
-					st.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
 		}
 	}
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery(					
+					"SELECT seller.*, department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"ORDER BY Name");
+			List<Seller> sellers = new ArrayList<Seller>();
+			Department department = null;
+			while(rs.next()) {
+				Integer id = rs.getInt("Id");
+				String name = rs.getString("Name");
+				String email = rs.getString("Email");
+				Date birthDate = rs.getDate("BirthDate");
+				Double baseSalary = rs.getDouble("BaseSalary");
+				if(department == null) {
+					String depName = rs.getString("DepName");
+					Integer departmentId = rs.getInt("DepartmentId");
+					department = new Department(departmentId, depName);
+				}
+				Seller seller = new Seller(id, name, email, birthDate, baseSalary, department);
+				sellers.add(seller);
+			}
+			return sellers;
+		}
+		catch(SQLException ex) {
+			throw new DbException(ex.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"WHERE DepartmentId = ? "
+					+"ORDER BY Name"
+					);
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			List<Seller> sellers = new ArrayList<Seller>();
+			while(rs.next()) {
+				Integer id = rs.getInt("Id");
+				String name = rs.getString("Name");
+				String email = rs.getString("Email");
+				Date birthDate = rs.getDate("BirthDate");
+				Double baseSalary = rs.getDouble("BaseSalary");
+				//String depName = rs.getString("DepName");
+				//Integer departmentId = rs.getInt("DepartmentId");
+				Seller seller = new Seller(id, name, email, birthDate, baseSalary, department);
+				sellers.add(seller);
+			}
+			return sellers;
+		}
+		catch(SQLException ex) {
+			throw new DbException(ex.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 }
