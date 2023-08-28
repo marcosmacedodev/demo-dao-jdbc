132dbee1416e04e6c6ebff9d666615049e3f0922
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-28 17:56:29 -0300

    Criado DAOs de Seller e Department e EntityDAO classe genérica

---
+++ src/daos/DepartmentDAO.java
@@ -0,0 +1,37 @@
+package daos;
+
+import java.util.List;
+
+import entities.Department;
+
+public class DepartmentDAO implements EntityDAO<Department>{
+
+	@Override
+	public void insert(Department entity) {
+		
+	}
+
+	@Override
+	public void update(Department entity) {
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+
+		return null;
+	}
+
+}
---
+++ src/daos/SellerDAO.java
@@ -0,0 +1,39 @@
+package daos;
+
+import java.util.List;
+
+import entities.Seller;
+
+public class SellerDAO implements EntityDAO<Seller>{
+
+	@Override
+	public void insert(Seller entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
